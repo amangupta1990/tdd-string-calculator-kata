4149f4bd8e1a33b1a9c1b18adf9cd662ca51e591
Aman gupta <amga@topsoe.com>
2024-10-16 12:19:59 +0530

    refactor: handle spaces

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -7,6 +7,6 @@ module.exports = function stringCalculator(input) {
         return parseInt(input);
     }
 
-    const numbers = input.split(/,|\n/);
+    const numbers = input.split(/[\n, ]/);
     return numbers.reduce((acc, number) => acc + parseInt(number), 0);
 }
