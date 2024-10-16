3b9310a1b75771bf44fdb1a49bfd6e5c3fdcf7f7
Aman gupta <amga@topsoe.com>
2024-10-16 11:55:43 +0530

    refactor: added helper method; more assertions

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -5,8 +5,14 @@ describe('String Calculator', () => {
         expect(stringCalculator('')).toBe(0);
     });
 
-    it('should return 1 if string is "1"', () => {
-        expect(stringCalculator('1')).toBe(1);
+    it('should return the same number if string has only one value', () => {
+        expect(resultEvaluator("1", 1));
+        expect(resultEvaluator("2 ", 2));
+        expect(resultEvaluator(" 3", 3));
     })
+
+    const resultEvaluator = (input, expected) => {
+        expect(stringCalculator(input)).toBe(expected);
+    }
 
 });
