4844bcf026e1f7a8e247618ef42c51fdf9463878
Aman gupta <amga@topsoe.com>
2024-10-16 13:27:15 +0530

    feat: add test for skip large number

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -35,6 +35,12 @@ describe('String Calculator', () => {
 
     })
 
+    it('should skip numbers bigger than 1000', () => {
+        expect(resultEvaluator("1001", 0));
+        expect(resultEvaluator("1000,1001", 1000));
+        expect(resultEvaluator("1001,1002", 0));
+    });
+
 
 
     const resultEvaluator = (input, expected) => {
