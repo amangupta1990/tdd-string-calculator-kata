e8d83d1d77fce73b328a444b554f55ca1d27a637
Aman Gupta <amangupta.1990@ymail.com>
2024-11-08 13:36:47 +0530

    chore: added eslint and prettier; default

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,65 +1,58 @@
 module.exports = function stringCalculator(stringInput) {
-    if (stringInput === '') {
-        return 0;
-    }
+  if (stringInput === "") {
+    return 0;
+  }
 
+  const numbersArray = getNumArrayFromString(stringInput);
 
-    const numbersArray = getNumArrayFromString(stringInput)
+  const negativeNumbers = filterNegatives(numbersArray);
+  if (negativeNumbers.length > 0) {
+    throw new Error(`Negatives not allowed: ${negativeNumbers.join(", ")}`);
+  }
 
-    const negativeNumbers = filterNegatives(numbersArray);
-    if (negativeNumbers.length > 0) {
-        throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
-    }
-
-    return filterLessThanThousand(numbersArray)
-        .reduce((acc, number) => acc + number, 0);
-}
-
+  return filterLessThanThosand(numbersArray).reduce(
+    (acc, number) => acc + number,
+    0
+  );
+};
 
 function getNumArrayFromString(numberString) {
+  let delimiter = null;
+  let numbers = null;
+  const defaultDelimiter = /,|\n|\s+/;
 
-    let delimiter = null;
-    let numbers = null;
-    const defaultDelimiter = /,|\n|\s+/;
+  const customDelimiter = getcustomDelimiter(numberString);
+  if (customDelimiter) {
+    delimiter = new RegExp(customDelimiter);
+    inputWithoutDelimiterLine = numberString.split("\n").slice(1).join("\n");
+    numbers = inputWithoutDelimiterLine;
+  } else {
+    delimiter = defaultDelimiter;
+    numbers = numberString;
+  }
 
-    const customDelimiter = getcustomDelimiter(numberString);
-    if (customDelimiter) {
-        delimiter = new RegExp(customDelimiter);
-        inputWithoutDelimiterLine = numberString.split('\n').slice(1).join('\n');
-        numbers = inputWithoutDelimiterLine;
-
-    }
-    else {
-        delimiter = defaultDelimiter;
-        numbers = numberString;
-    }
-
-    return numbers.split(delimiter).map(number => parseInt(number))
+  return numbers.split(delimiter).map((number) => parseInt(number));
 }
 
-
 function filterLessThanThousand(numberArray) {
-    return numberArray.filter(number => number <= 1000);
+  return numberArray.filter((number) => number <= 1000);
 }
 
 function filterNegatives(numbersArray) {
-    return numbersArray.filter(number => number < 0);
-
+  return numbersArray.filter((number) => number < 0);
 }
 
 function getcustomDelimiter(input) {
-    const customDelimiterMatch = input.match(/^\/\/(\[.+\])+\n/);
+  const customDelimiterMatch = input.match(/^\/\/(\[.+\])+\n/);
 
-    if (customDelimiterMatch) {
-        delimiter = customDelimiterMatch[0]
-            .slice(2, -1) // remove the slashes and new line
-            .split('][') // split the delimiters
-            .map(delimiter => delimiter.replace(/[\[\]]/g, '')) // remove the opening and closing bracket
+  if (customDelimiterMatch) {
+    delimiter = customDelimiterMatch[0]
+      .slice(2, -1) // remove the slashes and new line
+      .split("][") // split the delimiters
+      .map((delimiter) => delimiter.replace(/[\[\]]/g, "")); // remove the opening and closing bracket
 
-        combinedDelimiters = delimiter.join('|');
+    combinedDelimiters = delimiter.join("|");
 
-        return combinedDelimiters
-    }
-
-    else return null;
+    return combinedDelimiters;
+  } else return null;
 }
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,64 +1,58 @@
-const stringCalculator = require('../src/stringCalculator');
+const stringCalculator = require("../src/stringCalculator");
 
-describe('String Calculator', () => {
+describe("String Calculator", () => {
+  it("should return 0 if string is empty", () => {
+    expect(stringCalculator("")).toBe(0);
+  });
 
-    it('should return 0 if string is empty', () => {
-        expect(stringCalculator('')).toBe(0);
-    });
+  it("should return the same number if string has only one value", () => {
+    expect(resultEvaluator("1", 1));
+    expect(resultEvaluator("2 ", 2));
+    expect(resultEvaluator(" 3", 3));
+  });
 
-    it('should return the same number if string has only one value', () => {
-        expect(resultEvaluator("1", 1));
-        expect(resultEvaluator("2 ", 2));
-        expect(resultEvaluator(" 3", 3));
-    })
+  it("should return the sum of numbers separated by a comma", () => {
+    expect(resultEvaluator("1,2,3,4,5", 15));
+  });
 
-    it('should return the sum of numbers separated by a comma', () => {
-        expect(resultEvaluator("1,2,3,4,5", 15));
-    });
+  it("should return the sum of numbers separated by spaces", () => {
+    expect(resultEvaluator("5 10", 15));
+    expect(resultEvaluator("5   10", 15));
+  });
 
-    it('should return the sum of numbers separated by spaces', () => {
-        expect(resultEvaluator("5 10", 15));
-        expect(resultEvaluator("5   10", 15));
-    });
+  it("should return the sum of numbers separated by a comma and newlines", () => {
+    expect(resultEvaluator("1\n4,5", 10));
+  });
 
-    it('should return the sum of numbers separated by a comma and newlines', () => {
-        expect(resultEvaluator("1\n4,5", 10));
-    });
+  it("should use the delimiter specified in the first line", () => {
+    expect(resultEvaluator("//[;]\n1;2", 3));
+  });
 
-    it('should use the delimiter specified in the first line', () => {
-        expect(resultEvaluator("//[;]\n1;2", 3));
+  it("should use delimiters of any length", () => {
+    expect(resultEvaluator("//[;;]\n1;;2;;3", 6));
+  });
 
-    });
+  it("should use multiple delimiters", () => {
+    expect(resultEvaluator("//[;][,]\n1;2,3", 6));
+  });
 
-    it('should use delimiters of any length', () => {
-        expect(resultEvaluator("//[;;]\n1;;2;;3", 6));
-    });
+  it("should use multiple delimiters of any length", () => {
+    expect(resultEvaluator("//[;;][,]\n1;;2,3", 6));
+  });
 
-    it('should use multiple delimiters', () => {
-        expect(resultEvaluator("//[;][,]\n1;2,3", 6));
-    })
+  it("show throw an exception if a negative number(s) is found", () => {
+    expect(() => stringCalculator("-1")).toThrow("Negatives not allowed: -1");
+    expect(() => stringCalculator("1,-2")).toThrow("Negatives not allowed: -2");
+  });
 
-    it('should use multiple delimiters of any length', () => {
-        expect(resultEvaluator("//[;;][,]\n1;;2,3", 6));
-    })
+  it("should skip numbers bigger than 1000", () => {
+    expect(resultEvaluator("1001", 0));
+    expect(resultEvaluator("1000,1001", 1000));
+    expect(resultEvaluator("1001,1002", 0));
+    expect(resultEvaluator("1000,2,1001,1002,3", 1005));
+  });
 
-    it('show throw an exception if a negative number(s) is found', () => {
-        expect(() => stringCalculator("-1")).toThrow('Negatives not allowed: -1');
-        expect(() => stringCalculator("1,-2")).toThrow('Negatives not allowed: -2');
-
-    })
-
-    it('should skip numbers bigger than 1000', () => {
-        expect(resultEvaluator("1001", 0));
-        expect(resultEvaluator("1000,1001", 1000));
-        expect(resultEvaluator("1001,1002", 0));
-        expect(resultEvaluator("1000,2,1001,1002,3", 1005));
-    });
-
-
-
-    const resultEvaluator = (input, expected) => {
-        expect(stringCalculator(input)).toBe(expected);
-    }
-
+  const resultEvaluator = (input, expected) => {
+    expect(stringCalculator(input)).toBe(expected);
+  };
 });
